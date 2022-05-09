99f8b14a3617e43f9dfa6845ba290146217cda79
aleem aheer <aleemaheer5769644@gmail.com>
2022-05-09 21:10:49 +0500

    Added a function in play.go to check that the number is prime number or not

--- even_odd_app/play.go
+++ even_odd_app/play.go
@@ -6,10 +6,12 @@ func main() {
 	var num int
 	fmt.Println("Enter a number to check: ")
 	fmt.Scanln(&num)
-	res := check(num)
-	fact := factorial(num)
+	res := check(num)          // This function will check the number is even or odd
+	fact := factorial(num)     // This function will find the factorial of a number
+	prime_result := prime(num) // This function will check that the number is even or odd
 	fmt.Println("Result: ", res)
 	fmt.Println("Factorial: ", fact)
+	fmt.Println("IsEven: ", prime_result)
 }
 
 func check(n int) string {
@@ -27,4 +29,14 @@ func factorial(n int) int {
 		result = result * i
 	}
 	return result
+}
+
+func prime(n int) string {
+	var i int
+	for i = n - 1; i > 1; i-- {
+		if n%i == 0 {
+			return "Not a Prime Number"
+		}
+	}
+	return "Prime number"
 }
